a0810e238d6e428d1906d9d9329a2d6941adfc13
Adhi Venkatesh <adhivenkatesh32@gmail.com>
2021-05-08 12:13:38 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace AsyncAwaitCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            callmethod();
+            Console.WriteLine("Hello World!");
+        }
+
+        static async void callmethod()
+        {
+            Task<int> _task = Method1();
+            Method2();
+
+            int count = await _task;
+
+            Method3(count);
+        }
+
+        public static async Task<int> Method1()
+        {
+            int count = 0;
+
+            await Task.Run(() =>
+            {
+
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine("--> METHOD1");
+                }
+            });
+
+            return count;
+        }
+
+
+        public static void Method2()
+        {
+            for (int i = 0; i < 20; i++)
+            {
+                Console.WriteLine("----------> METHOD2");
+            }
+        }
+
+        public static void Method3(int count)
+        {
+            
+                Console.WriteLine("------> METHOD3");
+            
+        }
+
+    }
+}
